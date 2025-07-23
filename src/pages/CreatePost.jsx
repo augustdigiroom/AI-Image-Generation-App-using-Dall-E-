d7fcdf0cdd3567c9df1122b226f4c88562d02ce3
augustinegrepo <augustinegrepo@gmail.com>
2025-07-23 15:15:11 +0800

    Finished the Create Post.jsx for the front end

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -16,16 +16,19 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setloading] = useState(false);
 
+  const generateImage = () => {
+
+  }
+
   const handleSubmit = () => {
     // Submit logic here
   };
 
-  const handleChange = (e) => {
-    // Change logic here
-  };
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
   const handleSurpriseMe = () => {
-    // Surprise logic here
+     const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
   };
 
   return (
@@ -42,7 +45,7 @@ const CreatePost = () => {
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormField
-            LabelName="Your name"
+            labelName="Your name"
             type="text"
             name="name"
             placeholder="John Doe"
@@ -50,7 +53,7 @@ const CreatePost = () => {
             handleChange={handleChange}
           />
           <FormField
-            LabelName="Prompt"
+            labelName="Prompt"
             type="text"
             name="prompt"
             placeholder="A plush toy robot sitting against a yellow wall"
@@ -74,8 +77,36 @@ const CreatePost = () => {
                 className="w-9/12 h-9/12 object-contain opacity-40"
               />
             )}
+
+            {generatingImg && (
+
+                <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                    <Loader />
+                </div>
+            )}
           </div>
         </div>
+
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+
+            <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing...' : 'Share with the Community'}
+          </button>
+        </div>
+
       </form>
     </>
   );
